4ee1cd1f1f4de0fc3b3301bbad533e813523fcc0
Weston <barnesweston@gmail.com>
2022-06-28 18:49:32 -0500

    create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return (
+        <section className="header">
+            <header>
+                <h1 className="header-title">Hello there.</h1>
+                <div className="header-box">
+                    I'm Weston, a Full-Stack developer based in Tennessee, USA!
+                </div>
+            </header>
+        </section>
+
+    )
+}
+
+export default Header;
