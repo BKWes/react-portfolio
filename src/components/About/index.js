90e3b1c9a6ece553539409f584ac52c1845531ab
Weston <barnesweston@gmail.com>
2022-06-28 19:14:30 -0500

    create basic about component

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+    return (
+        <section className="about">
+            <span>
+                I became interseted in coding while I was a teenager, using basic HTML and CSS. I currently work with JavaScript, Node, and React. 
+            </span>
+        </section>
+    )
+}
+
+export default About;
