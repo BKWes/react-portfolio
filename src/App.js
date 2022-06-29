6d0fc9067695577a304ca99b9e24638b25100a1c
Weston <barnesweston@gmail.com>
2022-06-28 19:45:45 -0500

    create basic footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import About from './components/About';
+import Footer from './components/Footer';
 import Header from './components/Header'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Header></Header>
         <About></About>
+        <Footer></Footer>
       </main>
     </div>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>
+                You can find my work on <a href="https://www.github.com/BKWes" target="_blank" rel="noreferrer"><span className="footerLink"> Github. </span></a>
+                Lets connect on <a href="https://www.linkedin.com/in/weston-barnes-202947149/" target="_blank" rel="noreferrer"><span className="footerLink"> LinkedIn </span></a>            
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
