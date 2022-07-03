4c427bd56332274a7873562c6a9156b6e9f3868c
Weston <barnesweston@gmail.com>
2022-07-03 16:26:33 -0500

    add portfolio section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <section className="about">
             <h3>
-                A software developer specialized in JavaScript and the MERN stack. 
+                I'm Weston, a software developer who specializes in JavaScript and the MERN stack. 
             </h3>
             <span>
                 I am highly adaptable and always willing to learn through the creation of efficient and dynamic webapps.
